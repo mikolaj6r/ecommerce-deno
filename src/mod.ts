12d430e411ba70a271a95d33872b48da97aa73a3
mikolaj6r <mikolaj6r@gmail.com>
2020-05-27 17:10:56 +0200

    chore: add oak and denojuncks

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,12 @@
-import { serve } from "https://deno.land/std/http/server.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
+
+import { Application, Router, send, Context } from "https://deno.land/x/oak/mod.ts";
+import {
+  viewEngine,
+  engineFactory,
+  adapterFactory,
+  ViewConfig
+} from "https://deno.land/x/view_engine/mod.ts";
 
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(Deno.args).port;
@@ -10,10 +17,35 @@ if (isNaN(port)) {
   Deno.exit(1);
 }
 
-const body = new TextEncoder().encode("Hello World\n");
-const s = serve({ port: port, hostname: '0.0.0.0'});
-console.log("http://localhost:" + port);
+const app = new Application();
+const router = new Router();
 
-for await (const req of s) {
-  req.respond({ body });
+const denjuckEngine = await engineFactory.getDenjuckEngine();
+const oakAdapter = await adapterFactory.getOakAdapter();
+
+const viewConfig: ViewConfig = {
+  viewRoot: "./views",
 }
+
+// simple static files server
+app.use(async (ctx, next) => {
+  await send(ctx, ctx.request.url.pathname, {
+    root: `${Deno.cwd()}/static`,
+  });
+  next();
+});
+
+// view middleware
+app.use(viewEngine(oakAdapter, denjuckEngine, viewConfig));
+
+
+router.get("/", (ctx) => {
+  ctx.render("index.html", { data: { msg: "World" } });
+});
+
+//Adding middleware to require our router
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("App is listening to port: 8000");
+await app.listen({ port: 8000 });
