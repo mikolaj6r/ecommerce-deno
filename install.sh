9881f010cc3704b1ba508749cf1762ba920a6c63
mikolaj6r <mikolaj6r@gmail.com>
2021-06-21 17:02:22 +0200

    build: force install deno v1.1.1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 export DENO_INSTALL=$HOME/.deno
 export PATH=$DENO_INSTALL/bin:$PATH
-curl -fsSL https://deno.land/x/install/install.sh | sh
+curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.1.1
 deno --version
 
 npm install heroku -g
